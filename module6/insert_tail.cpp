f44202a38d9ddc20a061ee304913950d9f5a75da
mehedi-imun <mehediimun@gmail.com>
2024-06-28 21:55:38 +0600

    add option condaion for input and print and terminte loop

--- module6/insert_tail.cpp
+++ module6/insert_tail.cpp
@@ -26,15 +26,53 @@ void insertATail(Node *&head, int v)
         tmp = tmp->next;
     }
     tmp->next = newNode;
-    
 };
+void printList(Node *head)
+{
+    cout << "List: ";
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->value << " ";
+        tmp = tmp->next;
+    }
+}
 int main()
 {
     Node *head = NULL;
-    int option;
-    cin >> option;
-    if (option == 1)
+    while (true)
     {
+        cout << "option 1: Insert a value at the tail" << endl;
+        cout << "option 2: print the list" << endl;
+        cout << "option 3: exit" << endl;
+        int option;
+        cin >> option;
+        if (option == 1)
+        {
+            cout << "input value:";
+            int value;
+            cin >> value;
+            insertATail(head, value);
+        }
+        else if (option == 2)
+        {
+            Node *tmp = head;
+            while (tmp != NULL)
+            {
+                cout << tmp->value << " ";
+                tmp = tmp->next;
+            }
+            cout << endl;
+        }
+        else if (option == 3)
+        {
+            break;
+        }
+        else
+        {
+            cout << "invalid option" << endl;
+        }
     }
+
     return 0;
 }
