78cff55751e93ee38d50639253b60c3008e39e11
mehedi-imun <mehediimun@gmail.com>
2024-07-08 10:23:57 +0600

    solve reverse prist list wihh recersation

---
+++ module7.5/reverse_link_list.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+void insert_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+void printList(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->value << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void reversePrintList(Node* Head){
+    if(Head ==NULL){
+        return;
+    }
+    reversePrintList(Head->next);
+    cout<<Head->value<<" ";
+}
+int main()
+{
+    Node *head1 = NULL;
+    Node *tail1 = NULL;
+    int v1;
+    while (true)
+    {
+        cin >> v1;
+        if (v1 == -1)
+        {
+            break;
+        }
+        insert_tail(head1, tail1, v1);
+    }
+  reversePrintList(head1);
+    return 0;
+}
