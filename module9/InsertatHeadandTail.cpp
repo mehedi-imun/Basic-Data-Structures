bee816af2325e5d03e2e82d5a5333a1a36d73b4b
mehedi imun <mehediimun@gmail.com>
2024-07-10 15:02:18 +0600

    learn dubley list insert head and tail

---
+++ module9/InsertatHeadandTail.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+int main()
+{
+
+    return 0;
+}
