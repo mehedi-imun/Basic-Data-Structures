28baf4f2d8c57dba13de540c7c5e2ebc4dc98193
mehedi imun <mehediimun@gmail.com>
2024-07-12 11:35:30 +0600

    learn list bultin funtion list

---
+++ module10/list_constructors.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    list <int> myList={12,20,25};
+
+    for (int li:myList)
+    {
+        cout << li<< endl;
+    }
+    
+
+    return 0;
+}
